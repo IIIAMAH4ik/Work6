54d1a69adc58844e2330796f00861a59e0768272
Tembulat Tokmakov <tembatokmakov43@gmail.com>
2023-02-04 20:09:29 +0300

    commit

--- src/main/java/ru/netology/sqr/squaring/services/SQRService.java
+++ src/main/java/ru/netology/sqr/squaring/services/SQRService.java
@@ -1,12 +1,19 @@
 package ru.netology.sqr.squaring.services;
+
+import java.sql.SQLOutput;
+
 public class SQRService {
     public long calculate(long amount, boolean registered) {
-        int percent = registered ? 3 : 1;
-        long bonus = amount * percent / 100;
-        long limit = 500;
-        if (bonus > limit) {
-            bonus = limit;
+        int range = 10;
+        int min = 200;
+        int max = 300;
+        int sqrScore=0;
+        while (range<100) {
+        if (range * range >= min && range * range <= max) {
+           sqrScore = sqrScore +1;
         }
-        return bonus;
+        range = range +1;
+        }
+        return amount;
     }
 }
