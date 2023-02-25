737646f49b95e6b4a973a872e7a20cee747628a0
Tembulat Tokmakov <tembatokmakov43@gmail.com>
2023-02-25 14:53:45 +0300

    final commit

--- src/test/java/ru/netology/sqr/squaring/services/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/squaring/services/SQRServiceTest.java
@@ -1,18 +1,20 @@
 package ru.netology.sqr.squaring.services;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.netology.sqr.squaring.services.SQRService;
 
 
 import static org.junit.jupiter.api.Assertions.*;
-    class SQRServiceTest {
-        @Test
-        public void testCalcWhenSomeSquaresFound() {
-            SQRService service = new SQRService();
 
-           int actual = service.calc(200,300);
-           int expected = 3;
-            Assertions.assertEquals(expected, actual);
-        }
+class SQRServiceTest {
+    @Test
+    public void testCalcWhenSomeSquaresFound() {
+        SQRService service = new SQRService();
 
+        int actual = service.calc(200, 300);
+        int expected = 3;
+        Assertions.assertEquals(expected, actual);
     }
+
+}
